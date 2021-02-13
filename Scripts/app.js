449404b1876b14968ee3729b84a933305d96fcc9
Javid <j-n_01@hotmail.com>
2021-02-13 09:59:37 -0500

    code cleanup, Added AOS, Misc Fixes

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,34 +1,20 @@
- //SideNav
- const sideNav = document.querySelector('.sidenav');
- M.Sidenav.init(sideNav, {});
 
- //Slider Initialization
- const slider = document.querySelector('.slider');
- M.Slider.init(slider, {
-   indicators: false,
-   height: 500,
-   transition: 200,
-   interval: 6000
- });
-
-
- // Material Boxed
+// Material Boxed - Enlarge Images 
  const mb = document.querySelectorAll('.materialboxed');
  M.Materialbox.init(mb, {});
 
  //ScrollSpy 
  const scrollSpy = document.querySelectorAll('.scrollspy');
- M.ScrollSpy.init(scrollSpy, {});
+M.ScrollSpy.init(scrollSpy, {});
+ 
+
+// Animate a Smooth Scrooling
+var scroll = new SmoothScroll('a[href*="#" ]', {
+    speed: 1000
+});
 
 
- // New menu Nav 
- /*const menuToggle = document.querySelector('.toggle')
- const showcase = document.querySelector('.showcase')
 
- menuToggle.addEventListener('click', () => {
-   menuToggle.classList.toggle('active');
-   showcase.classList.toggle('active');
- })  */
 
  // Navbar Functionality (Opening & closing)
 const navToggle = document.querySelector('.nav-toggle');
@@ -45,6 +31,13 @@ navLinks.forEach(link => {
 })
 
 
+// Initialize Animate on Scroll - AOS
+AOS.init({
+    anchorPlacement: 'top-bottom',
+    delay: 250,
+    once: true,
 
 
-v
+  });
+
+
